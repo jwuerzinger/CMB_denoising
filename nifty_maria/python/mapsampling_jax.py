905904406d93fbcb52a39e0af1d16d25cbb9e05c
Jonas Wuerzinger <jwuerzin@odslserv02.t2.mpcdf.mpg.de>
2024-10-07 11:50:54 +0200

    adding jax mapsampling as module; first draft notebook for simultaneous atmosphere+map fit

---
+++ nifty_maria/python/mapsampling_jax.py
@@ -0,0 +1,116 @@
+import jax
+import numpy as np
+
+# jax compatible rewrite if beams.separably_filter
+@jax.jit
+def separably_filter(data, F, tol=1e-2, return_filter=False):
+    """
+    This is more efficient than 2d convolution
+    """
+
+    assert data.ndim == 2
+
+    u, s, v = jax.numpy.linalg.svd(F)
+    effective_filter = 0
+    filtered_image = 0
+
+    # pad data array to achieve mirroring effect
+    data = jax.numpy.column_stack([data[:,0], data, data[:, -1]]) # axis=1
+    data = jax.numpy.vstack([data[0], data, data[-1]]) # axis=0
+
+    for m in range(len(F)):
+        effective_filter += s[m] * u[:, m : m + 1] @ v[m : m + 1]
+        
+        # Turn kernels into shape (1, dim) to work in nd function
+        u_kernel = jax.numpy.broadcast_to(u[:, m], (1, u[:, m].size))
+        v_kernel = jax.numpy.broadcast_to(u[:, m], (1, v[m].size))
+        
+        filtered_image += s[m] * jax.scipy.signal.convolve(jax.scipy.signal.convolve(data, u_kernel.T, mode='same'), v_kernel, mode='same')[1:-1, 1:-1]
+
+    return (filtered_image, effective_filter) if return_filter else filtered_image
+
+@jax.jit
+def construct_beam_filter(fwhm, res, buffer=1):
+    """
+    Make a beam filter for an image.
+    """
+
+    # if beam_profile is None:
+    #     # beam_profile = lambda r, r0: np.where(r <= r0, 1., 0.)
+
+    #     # a top hat
+    #     def beam_profile(r, r0):
+    #         return np.exp(-((r / r0) ** 16))
+
+    filter_width = buffer * fwhm
+
+    # n_side = jax.numpy.maximum(filter_width / res, 3).astype(int) # n_side = 3
+
+    filter_side = jax.numpy.linspace(-filter_width / 2, filter_width / 2, 3)
+    X, Y = jax.numpy.meshgrid(filter_side, filter_side, indexing="ij")
+    R = jax.numpy.sqrt(jax.numpy.square(X) + jax.numpy.square(Y))
+    F = jax.numpy.exp(-((R / (fwhm / 2)) ** 16))
+
+    return F / F.sum()
+
+from maria.constants import k_B
+from maria.instrument import beams
+import maria
+
+instrument = maria.get_instrument('MUSTANG-2')
+
+@jax.jit
+def sample_maps(sim_truthmap, dx, dy, resolution, x_side, y_side):
+
+    data_map = jax.numpy.array(1e-16 * np.random.standard_normal(size=dx.shape))
+    pbar = instrument.bands
+
+    for band in pbar:
+        band_mask = instrument.dets.band_name == band.name
+
+        nu = jax.numpy.linspace(band.nu_min, band.nu_max, 64)
+        TRJ = jax.scipy.interpolate.RegularGridInterpolator(
+            (jax.numpy.array([100.]),),
+            sim_truthmap,
+            fill_value=None,
+            bounds_error=False,
+            method='nearest',
+        )(nu)
+
+
+        nu_passband = jax.numpy.exp(jax.numpy.log(0.5) * (2 * (nu - 90.) / 30.) ** 2)
+        power_map = (
+            1e12
+            * k_B
+            * jax.numpy.trapezoid(nu_passband[:, None, None] * TRJ, axis=0, x=1e9 * nu)
+        )
+
+        # nu is in GHz, f is in Hz
+        nu_fwhm = beams.compute_angular_fwhm(
+            # fwhm_0=sim_truthmap.instrument.dets.primary_size.mean(),
+            fwhm_0=instrument.dets.primary_size.mean(),
+            z=np.inf,
+            f=1e9 * band.center,
+        ) 
+        
+        nu_map_filter = construct_beam_filter(fwhm=nu_fwhm, res=resolution)
+        filtered_power_map = separably_filter(power_map, nu_map_filter)
+        
+        # jax:
+        map_power = jax.scipy.interpolate.RegularGridInterpolator(
+            # Need to invert x_side and y_side for jax interpolation:
+            (jax.numpy.flip(x_side), jax.numpy.flip(y_side)), # length N=2 sequence of arrays with grid coords
+            # jax.numpy.flip(sim_truthmap[0]),
+            jax.numpy.flip(filtered_power_map), # N=2-dimensional array specifying grid values (1000, 1000)
+            fill_value=0.,
+            bounds_error=False,
+            method="linear",
+        )((jax.numpy.array(dx[band_mask]), jax.numpy.array(dy[band_mask])))
+
+        # jax.debug.print("Total map power: {pwr}", pwr=map_power.sum())
+
+        # data["map"][band_mask] += map_power
+        data_map = data_map.at[band_mask].add(map_power)
+        
+    # return sim_truthmap.data["map"]
+    return data_map
