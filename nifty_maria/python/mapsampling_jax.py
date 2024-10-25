c36964b5cd11347dc52f6b06c6b56987e9f323b8
Jonas Wuerzinger <jwuerzin@odslserv02.t2.mpcdf.mpg.de>
2024-10-25 13:57:19 +0200

    fixing sample_maps!

--- nifty_maria/python/mapsampling_jax.py
+++ nifty_maria/python/mapsampling_jax.py
@@ -3,29 +3,34 @@ import numpy as np
 
 # jax compatible rewrite if beams.separably_filter
 @jax.jit
-def separably_filter(data, F, tol=1e-2, return_filter=False):
+def separably_filter_2d(data, F, tol=1e-2, return_filter=False):
     """
     This is more efficient than 2d convolution
     """
 
-    assert data.ndim == 2
+    if F.ndim != 2:
+        raise ValueError("F must be two-dimensional.")
 
     u, s, v = jax.numpy.linalg.svd(F)
     effective_filter = 0
     filtered_image = 0
 
-    # pad data array to achieve mirroring effect
-    data = jax.numpy.column_stack([data[:,0], data, data[:, -1]]) # axis=1
-    data = jax.numpy.vstack([data[0], data, data[-1]]) # axis=0
-
     for m in range(len(F)):
         effective_filter += s[m] * u[:, m : m + 1] @ v[m : m + 1]
         
-        # Turn kernels into shape (1, dim) to work in nd function
         u_kernel = jax.numpy.broadcast_to(u[:, m], (1, u[:, m].size))
-        v_kernel = jax.numpy.broadcast_to(u[:, m], (1, v[m].size))
-        
-        filtered_image += s[m] * jax.scipy.signal.convolve(jax.scipy.signal.convolve(data, u_kernel.T, mode='same'), v_kernel, mode='same')[1:-1, 1:-1]
+        v_kernel = jax.numpy.broadcast_to(v[m], (1, v[m].size))
+        # v_kernel = jax.numpy.broadcast_to(u[:, m], (1, v[m].size))
+
+        filtered_image += s[m] * jax.scipy.signal.convolve(
+            jax.scipy.signal.convolve(data[0], u_kernel.T, mode='same', precision='default'), 
+            v_kernel, mode='same', precision='default'
+            )
+
+        # if np.abs(F - effective_filter).mean() < tol: break # may not be necessary?!
+
+    # re-add 0th dim:
+    filtered_image = filtered_image[None, :, :]
 
     return (filtered_image, effective_filter) if return_filter else filtered_image
 
@@ -44,9 +49,11 @@ def construct_beam_filter(fwhm, res, buffer=1):
 
     filter_width = buffer * fwhm
 
-    # n_side = jax.numpy.maximum(filter_width / res, 3).astype(int) # n_side = 3
+    # n_side = jax.numpy.maximum(filter_width / res, 3).astype(int) # n_side = 27 -> having this as var breaks jit...
+    # filter_side = jax.numpy.linspace(-filter_width / 2, filter_width / 2, n_side)
 
-    filter_side = jax.numpy.linspace(-filter_width / 2, filter_width / 2, 3)
+    filter_side = jax.numpy.linspace(-filter_width / 2, filter_width / 2, 27)
+
     X, Y = jax.numpy.meshgrid(filter_side, filter_side, indexing="ij")
     R = jax.numpy.sqrt(jax.numpy.square(X) + jax.numpy.square(Y))
     F = jax.numpy.exp(-((R / (fwhm / 2)) ** 16))
@@ -86,6 +93,7 @@ def sample_maps(sim_truthmap, dx, dy, resolution, x_side, y_side):
         band_mask = instrument.dets.band_name == band.name
 
         nu = jax.numpy.linspace(band.nu_min, band.nu_max, 64)
+
         TRJ = jax.scipy.interpolate.RegularGridInterpolator(
             (jax.numpy.array([100.]),),
             sim_truthmap,
@@ -94,12 +102,13 @@ def sample_maps(sim_truthmap, dx, dy, resolution, x_side, y_side):
             method='nearest',
         )(nu)
 
-
+        TRJ = jax.numpy.reshape(TRJ, (1, 64, 1000, 1000)) # TODO: softcode this!
         nu_passband = jax.numpy.exp(jax.numpy.log(0.5) * (2 * (nu - 90.) / 30.) ** 2)
+
         power_map = (
             1e12
             * k_B
-            * jax.numpy.trapezoid(nu_passband[:, None, None] * TRJ, axis=0, x=1e9 * nu)
+            * jax.numpy.trapezoid(nu_passband[:, None, None] * TRJ, axis=1, x=1e9 * nu)
         )
 
         # nu is in GHz, f is in Hz
@@ -107,19 +116,20 @@ def sample_maps(sim_truthmap, dx, dy, resolution, x_side, y_side):
             # fwhm_0=sim_truthmap.instrument.dets.primary_size.mean(),
             fwhm_0=instrument.dets.primary_size.mean(),
             z=np.inf,
-            f=1e9 * band.center,
+            # f=1e9 * band.center,
+            nu = band.center
         ) 
         
         nu_map_filter = construct_beam_filter(fwhm=nu_fwhm, res=resolution)
-        filtered_power_map = separably_filter(power_map, nu_map_filter)
-        
-        # jax:
+        filtered_power_map = separably_filter_2d(power_map, nu_map_filter)
+
+        # assume no time-dim for now. TODO: Add time dim!
         map_power = jax.scipy.interpolate.RegularGridInterpolator(
             # Need to invert x_side and y_side for jax interpolation:
             # (jax.numpy.flip(x_side), jax.numpy.flip(y_side)), # length N=2 sequence of arrays with grid coords
             # jax.numpy.flip(filtered_power_map), # N=2-dimensional array specifying grid values (1000, 1000)
             (x_side, y_side), # length N=2 sequence of arrays with grid coords
-            filtered_power_map, # N=2-dimensional array specifying grid values (1000, 1000)
+            filtered_power_map[0], # N=2-dimensional array specifying grid values (1000, 1000)
             fill_value=0.,
             bounds_error=False,
             method="linear",
